692e7150115f8efffb7129bfb6cd681232c88307
Eric Bouchut <ebouchut@gmail.com>
2025-07-24 16:56:50 +0200

    feat: Add an example on how to use primitive data types and operators
    Add Lombok as a Maven dependency

--- src/main/java/com/ericbouchut/datatype/primitive/BankAccount.java
+++ src/main/java/com/ericbouchut/datatype/primitive/BankAccount.java
@@ -1,12 +1,61 @@
 package com.ericbouchut.datatype.primitive;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.sql.SQLOutput;
+
+@ToString
 public class BankAccount {
      protected String holderName;
 
-     char riskAversion = '\uFFFF';
-     byte age = 'A';
-     double balance;
+     @Getter
+     private char currency;
 
+     @Getter
+     private short age;
 
+     @Getter
+     private double balance;
 
+     private char[] code = {'A', 'B', 'C', '4', '2', '1'};
+
+     public BankAccount(String holderName, char curency, short age, double balance) {
+          this.holderName = holderName;
+          this.currency = currency;
+          this.age = age;
+          this.balance = balance;
+     }
+
+     /**
+      * Make a deposit of the specified amount.
+      * If the deposit failed the balance is unchanged.
+      *
+      * @param amount the amount to deposit to the account
+      * @return false if the deposit failed
+      */
+     public boolean deposit(double amount) {
+          balance += amount;
+
+          return true;
+     }
+
+     /**
+      * This one is the gold mine 🤩.
+      */
+     void jackpot() {
+          balance *= 2;
+     }
+
+     void birthday() {
+          age += 1;
+     }
+
+     protected void displayCode() {
+          for(int index = 0;  index < code.length;  index++) {
+               System.out.print( code[index]);
+          }
+          System.out.println();
+     }
 }
--- src/main/java/com/ericbouchut/datatype/primitive/Main.java
+++ src/main/java/com/ericbouchut/datatype/primitive/Main.java
@@ -4,8 +4,21 @@ package com.ericbouchut.datatype.primitive;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount();
+        BankAccount account = new BankAccount("Jack", '$', (short) 35, 1000D);
 
-        System.out.println(account.age);
+        System.out.println(account);
+
+        System.out.println("Birthday!");
+        account.birthday();
+        System.out.println("age: " + account.getAge());
+
+
+        System.out.println("Jackpot!");
+        account.jackpot();
+        System.out.println("account balance:" + account.getBalance());
+
+        System.out.println(account);
+
+        account.displayCode();
     }
 }
