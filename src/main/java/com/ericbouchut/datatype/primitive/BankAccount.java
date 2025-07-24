4c5004303eaace26d435eb9903391ffef6899364
Eric Bouchut <ebouchut@gmail.com>
2025-07-24 11:57:14 +0200

    feat: Add java source files

---
+++ src/main/java/com/ericbouchut/datatype/primitive/BankAccount.java
@@ -0,0 +1,12 @@
+package com.ericbouchut.datatype.primitive;
+
+public class BankAccount {
+     protected String holderName;
+
+     char riskAversion = '\uFFFF';
+     byte age = 'A';
+     double balance;
+
+
+
+}
